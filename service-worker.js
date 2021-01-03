4dd01fe79133e86430ee247f9712e67bbc516e19
Matt Wilber <mwilber@gmail.com>
2021-01-02 23:24:22 -0500

    Add dynamic cache to service worker.

--- service-worker.js
+++ service-worker.js
@@ -6,10 +6,14 @@
  * to build out your own caching strategy and other PWA features.
  */
 
+let CACHE_VERSION = '0.2';
+let CACHE_STATIC_NAME = 'static_v'+CACHE_VERSION;
+let CACHE_DYNAMIC_NAME = 'dynamic_v'+CACHE_VERSION;
+let cacheFirst = ['getCoverArt.view', 'download.view'];
+
 self.addEventListener('install', function(event){
     console.log('[SW] installing...');
-    caches.delete('static');
-    event.waitUntil(caches.open('static_v0.1')
+    event.waitUntil(caches.open(CACHE_STATIC_NAME)
         .then(function(cache){
             console.log('[SW] precaching');
             cache.addAll([
@@ -22,18 +26,62 @@ self.addEventListener('install', function(event){
 
 self.addEventListener('activate', function(event){
     console.log('[SW] activating...');
-    self.clients.claim();
+    // event.waitUntil(
+    //     caches.keys()
+    //         .then(function(keyList) {
+    //             return Promise.all(keyList.map(function(key) {
+    //                 if (key !== CACHE_STATIC_NAME) {
+    //                     return caches.delete(key);
+    //                 }
+    //             }));
+    //         })
+    // );
 });
 
 self.addEventListener('fetch', function(event){
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response){
-                if(response){
-                    return response;
-                }else{
-                    return fetch(event.request);
-                }
-            })
-    );
+    if( cacheFirst.some(v => event.request.url.includes(v)) ){
+        // Images and audio are always cache first
+        console.log('[SW] Cache First URL', event.request.url);
+        event.respondWith(
+            caches.match(event.request)
+                .then(function(response){
+                    if(response){
+                        console.log('[SW] Responding with cache');
+                        return response;
+                    }else{
+                        console.log('[SW] Punting to network');
+                        return fetch(event.request);
+                    }
+                })
+        );
+    }else{
+        // Default to network with cache fallback
+        event.respondWith(
+            fetch(event.request)
+                .then(function(res){
+                    // Skip caching the javascript bundle
+                    // TODO: remove this before production
+                    if( event.request.url.includes('bundle.js') ) return res;
+                    // Cache and return
+                    return caches.open(CACHE_DYNAMIC_NAME)
+                        .then(function(cache) {
+                            cache.put(event.request.url, res.clone());
+                            return res;
+                        });
+                })
+                .catch(function(err){
+                    // Network failed, try pulling from cache
+                    console.log('[SW] Network failed. Attempting cache.')
+                    return caches.match(event.request)
+                        .then(function(response){
+                            if(response){
+                                return response;
+                            }else{
+                                // TODO: decide if we need to handle a failed network request that isn't cached
+                                return;
+                            }
+                        });
+                })
+        );
+    }
 });
