61de62cd786b2a40057935aaf238b1feb4420327
Matt Wilber <mwilber@gmail.com>
2021-01-04 20:37:40 -0500

    async await caching

--- src/media-cache.js
+++ src/media-cache.js
@@ -1,6 +1,9 @@
 export class MediaCache{
 	constructor(){
-
+		this.reqCt = 0;
+		this.resCt = 0;
+		this.errCt = 0;
+		this.extCt = 0;
 	}
 
 	CachePlaylist(playlist){
@@ -10,38 +13,84 @@ export class MediaCache{
 		}, []);
 		console.log("🚀 ~ file: media-cache.js ~ line 11 ~ MediaCache ~ paths ~ paths", paths)
 		
-		let reqCt = 0;
-		let resCt = 0;
-		let errCt = 0;
-		let extCt = 0;
-		let cacheOut = document.querySelector('.cache-status');
+		this.reqCt = 0;
+		this.resCt = 0;
+		this.errCt = 0;
+		this.extCt = 0;
+		this.cacheOut = document.querySelector('.cache-status');
 
 		caches.open('playlist_'+playlist.name)
-			.then(function(cache){
+			.then((cache)=>{
 				paths.forEach((path)=>{
-					reqCt++;
-					caches.match(path).then((response)=>{
-						if(!response || !response.body){
-							// Not cached yet, so let's add it
-							cache.add(path).then((res)=>{
-								resCt++;
-								cacheOut.innerText = 'cached '+resCt+' of '+reqCt+'. '+extCt+' already cached. ( '+errCt+' errors ) '+(resCt+extCt+errCt);
-								//console.log('cached '+resCt+' of '+reqCt+'. '+extCt+' already cached. ( '+errCt+' errors )');
-							}).catch((err)=>{
-								errCt++;
-								cacheOut.innerText = 'cached '+resCt+' of '+reqCt+'. '+extCt+' already cached. ( '+errCt+' errors ) '+(resCt+extCt+errCt);
-								//console.error('something went wrong', err);
-							});
-						}else{
-							// Already in the cache
-							cacheOut.innerText = 'cached '+resCt+' of '+reqCt+'. '+extCt+' already cached. ( '+errCt+' errors ) '+(resCt+extCt+errCt);
-							extCt++;
+					this.reqCt++;
+					this.CacheResponse(path, cache).then((cacheResult)=>{
+						switch( cacheResult.type ){
+							case 'response':
+								this.resCt++;
+								this.cacheOut.innerText = 'cached '+this.resCt+' of '+this.reqCt+'. '+this.extCt+' already cached. ( '+this.errCt+' errors ) '+(this.resCt+this.extCt+this.errCt);
+								break;
+							case 'error':
+								this.errCt++;
+								this.cacheOut.innerText = 'cached '+this.resCt+' of '+this.reqCt+'. '+this.extCt+' already cached. ( '+this.errCt+' errors ) '+(this.resCt+this.extCt+this.errCt);
+								break;
+							case 'exist':
+								this.extCt++;
+								this.cacheOut.innerText = 'cached '+this.resCt+' of '+this.reqCt+'. '+this.extCt+' already cached. ( '+this.errCt+' errors ) '+(this.resCt+this.extCt+this.errCt);
+								break;
+							default:
 						}
 					});
+
+
+					// caches.match(path).then((response)=>{
+					// 	if(!response || !response.body){
+					// 		// Not cached yet, so let's add it
+					// 		cache.add(path).then((res)=>{
+					// 			this.resCt++;
+					// 			this.cacheOut.innerText = 'cached '+this.resCt+' of '+this.reqCt+'. '+this.extCt+' already cached. ( '+this.errCt+' errors ) '+(this.resCt+this.extCt+errCt);
+					// 			//console.log('cached '+this.resCt+' of '+this.reqCt+'. '+this.extCt+' already cached. ( '+this.errCt+' errors )');
+					// 		}).catch((err)=>{
+					// 			this.errCt++;
+					// 			this.cacheOut.innerText = 'cached '+this.resCt+' of '+this.reqCt+'. '+this.extCt+' already cached. ( '+this.errCt+' errors ) '+(this.resCt+this.extCt+errCt);
+					// 			//console.error('something went wrong', err);
+					// 		});
+					// 	}else{
+					// 		// Already in the cache
+					// 		this.cacheOut.innerText = 'cached '+this.resCt+' of '+this.reqCt+'. '+this.extCt+' already cached. ( '+this.errCt+' errors ) '+(this.resCt+this.extCt+errCt);
+					// 		this.extCt++;
+					// 	}
+					// });
 					
 				});
 				//cache.addAll(paths);
 				//.slice(92,93)
 			});
 	}
+
+	async CacheResponse(url, cache){
+		let response = await caches.match(url);
+		let result = {
+			type: 'error'
+		};
+		if(!response || !response.body){
+			try{
+				let res = await cache.add(url);
+				result.type = 'response';
+			}catch(err){
+				result.type = 'error';
+			}
+			await this.timeout(5000);
+		}else{
+			result.type = 'exist';
+		}
+
+		
+
+		return result;
+	}
+
+	timeout(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
+	
 }
