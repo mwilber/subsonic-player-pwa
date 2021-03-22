752201d73f0f263a2cd959b6e956c823fa8eed3a
Matt Wilber <mwilber@gmail.com>
2021-03-21 20:48:19 -0400

    Clean up media listing layout

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@
  * to build out your own caching strategy and other PWA features.
  */
 
-let CACHE_VERSION = '0.12';
+let CACHE_VERSION = '0.13';
 let MEDIA_CACHE_VERSION = '0.10';
 let CACHE_STATIC_NAME = 'static_v'+CACHE_VERSION;
 let CACHE_DYNAMIC_NAME = 'dynamic_v'+CACHE_VERSION;
